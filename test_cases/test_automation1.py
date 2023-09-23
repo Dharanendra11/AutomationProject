1a4199416ee230e0fbe208d8c15c8a5fedb927c2
dharanendra <dhranendragowda74@gmail.com>
2023-09-23 16:51:52 +0530

    test case modified.

--- test_cases/test_automation1.py
+++ test_cases/test_automation1.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,9 +9,20 @@ from selenium.webdriver.support.wait import WebDriverWait
 @pytest.mark.first
 def test_homepage():
     driver=webdriver.Chrome()
+
     driver.get("https://www.flipkart.com/")
     driver.maximize_window()
-    # wait= WebDriverWait(driver,60)
-    # wait.until(expected_conditions.visibility_of_element_located((By.XPATH,"//span[contains(text(),'Cart')]")))
+    time.sleep(10)
     print(driver.title)
     driver.quit()
+
+@pytest.mark.skip
+def test_home2():
+    driver=webdriver.Chrome()
+
+    driver.get("https://www.flipkart.com/")
+    driver.maximize_window()
+    print(driver.title)
+    driver.quit()
+
+    
