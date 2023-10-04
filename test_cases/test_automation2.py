afd892b75a3c1ba721e7852a3ac3d6e040408f73
dharanendra <dhranendragowda74@gmail.com>
2023-10-04 20:56:38 +0530

    removed cdp commands

--- test_cases/test_automation2.py
+++ test_cases/test_automation2.py
@@ -1,17 +1,8 @@
-import json
-from time import sleep
-import ordlookup
-
-from selenium import webdriver
-driver=webdriver.Chrome()
-
-driver.execute_cdp_cmd("Network.enable",cmd_args={})
-driver.execute_cdp_cmd("Network.responseReceived",cmd_args={})
-
-driver.get("http://google.com")
-
+import time
+import urllib
 import pytest
 from selenium import webdriver
+from urllib import parse
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
@@ -27,12 +18,20 @@ def setUp():
     print("Close browser")
 
 
-@pytest.mark.skip
-def test_home2(setUp):
+@pytest.mark.flipkart
+def test_home2():
     driver = webdriver.Chrome()
     driver.get("https://www.flipkart.com/")
     driver.maximize_window()
     print(driver.title)
+    current_url = driver.current_url
+    # parsed_url = urllib.parse.urlparse(current_url)
+    # protocol = parsed_url.scheme
+    # domain = parsed_url.netloc
+    new_url = urllib.parse.urljoin(current_url,'/cart')
+    driver.get(new_url)
+    time.sleep(5)
+
     driver.quit()
 
 #@pytest.mark.parameterize("test_input",expected,[("1+2",3),("2+6",10)])
